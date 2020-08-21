7fb263e347ffed87097b6690c224ee1af7541ee3
Alejandro Vidal <alejandro.vidal@iesdonana.org>
2020-08-21 18:15:42 +0200

    Add(): Now you can download mp3 or mp4

--- client/src/views/MainView.vue
+++ client/src/views/MainView.vue
@@ -1,16 +1,23 @@
 <template>
   <v-container fill-height>
     <v-row>
-      <v-col cols="12">
-        <v-form>
+      <v-col cols="6">
+        <v-form v-model="valid">
           <v-text-field
             v-model="url"
             label="Youtube Link"
-            required
+            :rules="[v => !!v || 'Item is required']"
           ></v-text-field>
-          <v-btn @click="downloadSong">
+          <v-select
+            :rules="[v => !!v || 'Item is required']"
+            :items="formatItems"
+            solo
+            label="Format"
+            v-model="format"
+          ></v-select>
+          <v-btn @click="generateLink" :disabled="!valid">
             <span v-if="!loading">
-              Descargar
+              Generar link
             </span>
             <v-progress-circular
               v-if="loading"
@@ -18,7 +25,12 @@
               color="primary"
             ></v-progress-circular>
           </v-btn>
+          <v-btn @click="downloadSong" v-if="ready">Descargar</v-btn>
         </v-form>
+      </v-col>
+      <v-col cols="6" class="text-center">
+        <span>{{ title }}</span>
+        <v-img class="mx-auto mt-3" max-width="50%" :src="imageUrl"></v-img>
       </v-col>
     </v-row>
   </v-container>
@@ -31,35 +43,49 @@ import Axios from 'axios'
 @Component
 export default class MainView extends Vue {
   public url: string = ''
-  public format: string = 'mp3'
+  public format: string = ''
   public serverUrl: string = 'http://localhost:3000'
   public loading: boolean = false
+  public imageUrl: string = ''
+  public title: string = ''
+  public ready: boolean = false
+  public formatItems: any[] = ['mp3', 'mp4']
+  public valid: boolean = false
 
-  public downloadSong() {
+  public generateLink() {
     this.loading = true
-    Axios.post(
-      this.serverUrl,
-      {
-        url: this.url,
-        format: this.format,
-      },
-      {
-        responseType: 'blob',
-      }
-    )
+    Axios.post(this.serverUrl, {
+      url: this.url,
+      format: this.format,
+    })
       .then(res => {
-        const url = window.URL.createObjectURL(new Blob([res.data]))
-        const link = document.createElement('a')
-        link.href = url
-        // TODO: Get song name
-        link.setAttribute('download', 'song.mp3')
-        document.body.appendChild(link)
-        link.click()
-        console.log(res)
+        this.imageUrl = res.data.info.videoDetails.thumbnail.thumbnails[3].url
+        this.title = res.data.info.videoDetails.title
       })
       .finally(() => {
         this.loading = false
+        this.ready = true
       })
+  }
+
+  public downloadSong() {
+    const name = this.title + '.' + this.format
+    console.log(name)
+    Axios.post(
+      this.serverUrl + '/download',
+      {
+        name: name,
+      },
+      { responseType: 'blob' }
+    ).then(res => {
+      const url = window.URL.createObjectURL(new Blob([res.data]))
+      const link = document.createElement('a')
+      link.href = url
+      link.setAttribute('download', name)
+      // link.setAttribute('download', name)
+      document.body.appendChild(link)
+      link.click()
+    })
   }
 }
 </script>
--- server/index.js
+++ server/index.js
@@ -3,41 +3,81 @@ const ytdl = require('ytdl-core')
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
-const mime = require('mime-types')
+// const ffmpeg = require('ffmpeg-static')
+// const ffmpeg = require('ffmpeg')
+const ffmpeg = require('fluent-ffmpeg')
 const { getInfo } = require('ytdl-core')
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('downloads'))
 app.use(morgan('dev'))
 
 app.post('/', (req, res) => {
   const url = req.body.url
   const format = req.body.format
   let name = ''
-  getInfo(url, (err, info) => {
-    name = (info.videoDetails.title + '.' + format).replace(' ', '_')
-    ytdl(url, {
-      quality: 'highestaudio',
-      filter: 'audioonly',
-    })
-      .pipe(fs.createWriteStream('downloads/' + name))
-      .on('close', () => {
-        const file = 'downloads/' + name
-        const mimetype = mime.lookup(file)
-        res.setHeader(
-          'Content-Disposition',
-          `      attachment; filename=${file}`
-        )
-        res.setHeader('Content-Type', mimetype)
-        res.download(file)
-        setTimeout(() => {
-          fs.unlinkSync(file)
-        }, 10000)
-        // res.status(200).json({ success: true })
+  if (format == 'mp3') {
+    getInfo(url, (err, info) => {
+      name = info.videoDetails.title + '.' + format
+      ytdl(url, {
+        quality: 'highestaudio',
+        filter: 'audioonly',
       })
-  })
+        .pipe(fs.createWriteStream('./downloads/' + name))
+        .on('close', () => {
+          res.status(200).json({
+            success: true,
+            filename: name,
+            info: info,
+          })
+        })
+    })
+  } else {
+    getInfo(url, (err, info) => {
+      name = info.videoDetails.title
+      ytdl(url, {
+        quality: 'highestaudio',
+        filter: 'audioonly',
+      }).pipe(fs.createWriteStream('./downloads/' + name + '.mp3'))
+      ytdl(url, {
+        quality: 'highestvideo',
+        filter: 'videoonly',
+      })
+        .pipe(fs.createWriteStream('./downloads/' + name + '.mp4'))
+        .on('close', () => {
+          new ffmpeg()
+            .addInput('./downloads/' + name + '.mp3')
+            .addInput('./downloads/' + name + '.mp4')
+            .on('error', function (err) {
+              console.log('An error occurred: ' + err.message)
+            })
+            .on('end', function () {
+              console.log('Processing finished !')
+              fs.unlinkSync('./downloads/' + name + '.mp4')
+              fs.unlinkSync('./downloads/' + name + '.mp3')
+              fs.renameSync(
+                './downloads/' + name + '1.mp4',
+                './downloads/' + name + '.mp4'
+              )
+              res.status(200).json({
+                success: true,
+                filename: name,
+                info: info,
+              })
+            })
+            .saveToFile('./downloads/' + name + '1.mp4')
+        })
+    })
+  }
+})
+
+app.post('/download', (req, res) => {
+  const name = req.body.name
+  const file = 'downloads/' + name
+  res.download(file)
 })
 
 app.listen(3000, () => {
